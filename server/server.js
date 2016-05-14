8e6af4f50976b9dd86ed5db791eb728eca2534ce
Brandon Mizuno <brandon.mizuno@gmail.com>
2016-05-14 14:40:31 -0700

    Progress on deferred requests. Need to fix indexedDB

--- server/server.js
+++ server/server.js
@@ -12,35 +12,11 @@ app.use(express.static(path.join(__dirname, '../client')));
 app.get('/messages', messageController.getMessages);
 
 app.post('/messages', messageController.postMessage);
-
-// app.get('/', function(req, res) {
-//   console.log('index');
+//
+// app.post('/button', function(req, res) {
 //   res.writeHead(200, {'Content-Type': 'text/html'});
-//   res.end(htmlFile);
+//   res.end("hello");
 // });
-//
-// app.get('*.js', function(req, res) {
-//   console.log("in server (js)", req.url);
-//   res.writeHead(200, {'Content-Type': 'text/javascript'});
-//   res.end(fs.readFileSync(path.join(__dirname, '../client', req.url)));
-// });
-//
-// app.get('*.html|*.png|*.css|*.mp4', function(req, res) {
-//   console.log("in server (other)", req.url);
-//   res.statusCode = 200;
-//   res.end(fs.readFileSync(path.join(__dirname, '../client', req.url)));
-// });
-//
-// app.get('*.ico', function(req, res) {
-//   console.log("ico", req.url);
-//   res.statusCode = 200;
-//   res.end(fs.readFileSync(path.join(__dirname, '../client/assets', req.url)));
-// });
-
-app.post('/button', function(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end("hello");
-});
 
 app.get('*', function(req, res) {
   console.log('caught:', req.url);
