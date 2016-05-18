4caf87c1a2b4bc8b7bf11a18c4f531397f537bb4
Masha Diminsky <mashadiminsky@Mashas-MacBook-Pro.local>
2016-05-18 15:23:42 -0700

    update

--- client/app.js
+++ client/app.js
@@ -1,5 +1,3 @@
-console.log("in app.js");
-
 skyport.cache([
   "/index.html",
   "/messages.html",
@@ -8,8 +6,11 @@ skyport.cache([
   "/style.css",
   "/assets/vid1.mp4",
   "/assets/vid3.mp4",
-  "/assets/lion.png",
-  // "/messages",
+  "/assets/birdy1.jpg",
+  "/assets/birdy2.gif",
+  "/assets/birdy3.gif",
+  // "https://code.jquery.com/ui/1.11.4/jquery-ui.min.js",
+  "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css",
   "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min.js"
 ]);
 
