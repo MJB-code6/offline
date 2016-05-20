0c3a915ccf1eaddb865f87524d868eb57dad20c7
Masha Diminsky <mashadiminsky@Mashas-MacBook-Pro.local>
2016-05-19 20:02:13 -0700

    created functions for reseting cache, indexedb, and sw. Need to fix some minor issues

--- client/skyport.js
+++ client/skyport.js
@@ -1,4 +1,14 @@
 (function() {
+/*function to reset indexDB, cache, and SW
+make it so if there are no arguments then it automatically clears all
+otherwise they provide an argument it will only clear whatevr argument they provide.
+
+1. clear cache
+2. clear index DB
+3. register new SW?
+4. clear all 3 if no arguments
+5. clear only item that is in arguments*/
+	
   //  Service Workers are not (yet) supported by all browsers
   if (!navigator.serviceWorker) return;
 
@@ -19,6 +29,8 @@
 
       //  Tell the service worker to create storage in indexedDB
       sendToSW({command: 'createDB', info: window.location.origin});
+			
+			sendToSW({command: 'registration', info: registration});
     });
   }
 
@@ -62,6 +74,61 @@
       });
     }
   };
+	
+	// IDEA: FUNCTION FOR CLEARING ONLY SPECIFIC ITEMS IN EACH CHOICE? 
+	// OR ADD MORE COMPLEXITY TO resetSaved()
+	// if empty will reset everything in all 3 possibilities
+	// or include what you want to reset resetSaved:
+	// //(cache(required), itemInCacheToReset, indexdb(required), dbName(required), sw, whichSWToReset(if more than 1 running));
+	//If they include one item with specifics, it will assume you want to clear the entire item. So 'cache' without 'itemInCacheToReset' will reset entire cache while indexdb will only reset the item in indexdb if included in parameters:(cache, indexdb, dbName, sw, whichSWToReset(if more than 1 running)); 
+	// dbName
+	
+	// FUNCTION FOR CLEARING ALL ITEMS IN EACH CHOICE:
+	// choose the data you want to reset out of 3: sw, cache, or indexdb.
+	function resetSaved() {
+  // get all arguments entered into function
+  var args = Array.prototype.slice.call(arguments);
+  var toReset = [];
+  
+	// if no parameters will assume you want to reset sw, cache, and indexdb.
+  if(args.length === 0) { 
+    toReset.push(
+			{command: "reset-cache"},
+			{command: "reset-indexdb"},
+			{command: "reset-sw"}
+		);
+  }else if(args.length > 0) {
+   	// loop through function, if particular argument exists then send
+    for(var i=0; i<args.length; i++) {
+      if(args[i] === "cache") { 
+      	toReset.push({command: "reset-cache"});
+      }else if(args[i] === "indexdb" && args[i] !== args[args.length-1] && args[i+1] !== "sw") { 
+        toReset.push({command: "reset-indexdb", info: args[i+1]});
+      }else if(args[i] === "sw") { 
+        toReset.push({command: "reset-sw"});
+//				navigator.serviceWorker.getRegistrations().then(function(registrations) {
+//					console.log(registrations);
+//					console.log("yeah1");
+//					for(var registration in registrations) {
+//						console.log("yeah2");
+//						var sw = registrations[registration];
+//						// console.log(registrations[registration]);
+//						sw.unregister().then(function(boolean) {
+//							console.log("worked!");
+//    				});
+//					}
+//				});
+      } 
+    }
+  }
+		
+  // if none of the items mentioned match any of the three possible items return
+  if(toReset.length === 0) { return };
+  
+		console.log('toReset: ', toReset)
+  // send array of object commands to be cleared to SW
+  sendToSW(toReset);
+}
 
   window.addEventListener('online', function(event) {
     sendToSW({command: "online", info: true});
@@ -104,14 +171,16 @@
     };
   }
 
-  function sendToSW(messageObj) {
+  function sendToSW(messageObjOrArr) {
     if (!serviceWorker) {
       navigator.serviceWorker.oncontrollerchange = function() {
         serviceWorker = navigator.serviceWorker.controller;
-        serviceWorker.postMessage(messageObj);
+        serviceWorker.postMessage(messageObjOrArr);
       }
     } else {
-      serviceWorker.postMessage(messageObj);
+      serviceWorker.postMessage(messageObjOrArr);
     }
   }
+	
+	resetSaved('sw');  
 })();
--- client/sw-skyport.js
+++ client/sw-skyport.js
@@ -1,8 +1,20 @@
+'use strict';
+/*function to reset indexDB, cache, and SW
+make it so if there are no arguments then it automatically clears all
+otherwise they provide an argument it will only clear whatevr argument they provide.
+
+1. clear cache
+2. clear index DB
+3. register new SW?
+4. clear all 3 if no arguments
+5. clear only item that is in arguments*/
+
 const CACHE_FIRST = 'precache';
 const FALLBACK_CACHE = 'fallback';
 var online = true;
 var db;
 var precacheAssets = precacheAssets || [];
+var registration;
 
 self.addEventListener('install', function(event) {
   return self.skipWaiting();
@@ -24,7 +36,6 @@ self.addEventListener('fetch', function(event) {
   });
 
 self.addEventListener('message', function(event) {
-
 	if (event.data.command === "cache") {
     precacheAssets = precacheAssets.concat(event.data.info);
     caches.open('precache')
@@ -89,5 +100,69 @@ self.addEventListener('message', function(event) {
       };
     }
   }
+	
+	if(event.data.command === "registration") {
+		registration = event.data.info;
+		console.log("registration", registration);
+	}
+	
+	// it's logging twice
+	for(var i=0; i<event.data.length; i++) {
+		if(event.data[i].command === "reset-cache") {
+			console.log("in reset-cache!");
+			caches.keys().then(function(cacheNames) {
+				return Promise.all(
+					cacheNames.filter(function(cacheName) {
+						return caches.delete(cacheName)
+					})
+				);
+			});
+		}
+
+		// deletes indexDB but requires page refresh to see
+		if(event.data[i].command === "reset-indexedb") {
+			console.log("in reset-indexedb!");
+			db.close();
+			var deleteReq = indexedDB.deleteDatabase(event.data[i].info);
+			
+			deleteReq.onerror = function() {
+				console.log("Error deleting database.");
+			};
+			
+			deleteReq.onsuccess= function(event) { 
+				console.log("Successfully deleted database!");
+				setTimeout(function() {
+					// setting false so it reloads from cache
+					// true from server
+					// this is not working, bug in chrome.
+        	window.location.reload(false);
+			 	}, 1000);
+			};
+			
+			deleteReq.onblocked = function () {
+				console.log("Couldn't delete database, operation blocked.");
+			};
+		}
+
+		// postMessage seems to be sending, but the console logs inside are not rendering and the sw is giving me weird errors from long ago.
+		if(event.data[i].command === "reset-sw") {
+			console.log("in reset-sw!");
+			// I sent postmessage from when SW register in skyport.js so it would be the same as saying 
+			// navigator.serviceWorker in the other file. It works in the skyport file (nav.SW) but as registration in here it doesn't run. 
+			// Also the 'message' listener is not receiving the console.log's from postMessage, yet it seems as if it's actually running the code inside. What...?
+			registration.getRegistrations().then(function(registrations) {
+				console.log(registrations);
+				console.log("yeah1");
+				for(var registration in registrations) {
+					console.log("yeah2");
+					var sw = registrations[registration];
+					// console.log(registrations[registration]);
+					sw.unregister().then(function(boolean) {
+						console.log("worked!");
+					});
+				}
+			});
+		}
+	}
 
 });
