21b892b77882e1e5c52fbb1038b0ce781d677d1e
Zhouxiang Huang <zhouxiang.huang@gmail.com>
2016-05-24 12:19:44 -0700

    test video storage

--- client/app.js
+++ client/app.js
@@ -4,11 +4,11 @@ skyport.cache([
   "/app.js",
   "/msgApp.js",
   "/style.css",
-  "/assets/vid1.mp4",
-  "/assets/vid3.mp4",
-  "/assets/birdy1.jpg",
-  "/assets/birdy2.gif",
-  "/assets/birdy3.gif",
+  // "/assets/vid1.mp4",
+  // "/assets/vid3.mp4",
+  // "/assets/birdy1.jpg",
+  // "/assets/birdy2.gif",
+  // "/assets/birdy3.gif",
   // "https://code.jquery.com/ui/1.11.4/jquery-ui.min.js",
   "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css",
   "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min.js"
@@ -16,4 +16,4 @@ skyport.cache([
 
 // skyport.fallback("/messages");
 
-skyport.dynamic(["/messages.html"]);
+skyport.dynamic(["/assets/vid1.mp4"]);
